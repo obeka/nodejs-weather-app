a0c7a99374d232b3292dfdf4df44448230e9b8b9
Ömer <omerbkk06@gmail.com>
2020-01-20 16:20:15 +0100

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 const port = process.env.PORT || 3000;
 
-//Setup directory for static files
-app.use(express.static(publicDirectory)) /* Static dosyalar için (html, css, js ...) */
-
 //Define paths for Express config
 const publicDirectory = path.join(__dirname, '../public');
 const viewPath = path.join(__dirname,'../templates/views');
-const partialsPath = path.join(__dirname, '../templates/partials')
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//Setup directory for static files
+app.use(express.static(publicDirectory)) /* Static dosyalar için (html, css, js ...) */
 
 
 //Setup handlesbars
